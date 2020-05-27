84501121d228b94afc39c8fcd6836ae3889d92e5
Hang Ho <bemoon0103@gmail.com>
2020-05-26 18:53:43 -0700

    add gitignore

--- src/components/textBox/index.js
+++ src/components/textBox/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
- 
+
 const TextBox = (props) => {
-    return <input type="text" name="search" value={props.search} onChange={props.handleInputChange}/>
+    return (
+        <div>
+            <br/>
+            <input type="text" placeholder="Search by first name" className="rounded" name="search" value={props.search} onChange={props.handleInputChange} />
+            </div>
+    )
 }
 
- export default TextBox;
+export default TextBox;
