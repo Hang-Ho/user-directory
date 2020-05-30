897b3fd137bec3801343b7e572bfff6b28c3d340
Hang Ho <bemoon0103@gmail.com>
2020-05-29 21:38:36 -0700

    changed App

---
+++ build/precache-manifest.7286f918a4ce6c391cc66e76c37963d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63b2e9481567fefd43dfa727ed45c08",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "be24778df831c9a88aea",
+    "url": "/user-directory/static/js/2.c83a77f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/user-directory/static/js/2.c83a77f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e866cab9d5ed212057",
+    "url": "/user-directory/static/js/main.aaa3af64.chunk.js"
+  },
+  {
+    "revision": "1c57bf3eb1d12d741648",
+    "url": "/user-directory/static/js/runtime-main.229aca1a.js"
+  }
+]);
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import EmployeeList from "../../containers/EmployeeList";
 
-const App = props => (
-    <Router>
-        <div className='container'>
-            <Route exact path='/' component={EmployeeList}/>
-        </div>
-    </Router>
-);
+const App = props => {
+
+    return <EmployeeList/>
+    // <Router>
+    //     <div className='container'>
+    //         <Route exact path='/' component={EmployeeList}/>
+    //     </div>
+    // </Router>
+}
+;
 
 export default App;
